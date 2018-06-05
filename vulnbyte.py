4c15cf0ab5091918b9d7beb4121cf9e139dc21a9
ApacheIsDead <38152394+ApacheIsDead@users.noreply.github.com>
2018-06-05 18:10:05 -0400

    Update vulnbyte.py

--- vulnbyte.py
+++ vulnbyte.py
@@ -8,12 +8,14 @@
 
 import subprocess
 import os
+import smtplib
 import time
 
-print("VULNBYTE VULNERABILITY ACCESOR\n\n")
+print("VULNBYTE TOOLKIT\n\n")
 # Below the code starts
-installdir = raw_input("Full Directory of script installation with script name included?: ")
-selection_item = raw_input("[1] - Anonymous Front Page Login Scan!\n[2] - Map your network!\n[3] - Scan a website for vulnerable queries to sql injection!\n[4] - Open SQLMap\n[5] - open msfconsole\n[6] - Change your mac address\n[7] - Open Social Engeneering toolkit\n[8] - Deauth a system by MAC address\n[9] - Tries to execute a remote code vulnerability in wordpress CM plugin\n[10] - Eternal Blue Exploit Scanner\n[11] - Get Dependencies Info\n")
+
+
+selection_item = raw_input("[1] - Anonymous Front Page Login Scan!\n[2] - Map your network!\n[3] - Scan a website for vulnerable queries to sql injection!\n[4] - Open SQLMap\n[5] - open msfconsole\n[6] - Change your mac address\n[7] - Open Social Engeneering toolkit\n[8] - Deauth a system by MAC address\n[9] - Tries to execute a remote code vulnerability in wordpress CM plugin\n[10] - Eternal Blue Exploit Scanner\n[11] - Get Dependencies Info\n[12] - Install xerosploit\n[13] - Git Script Installer\n[14] - Ping A Target To Check If Online\n[15] - Traceroute a target to get hostname\n[16] - DNS Scanner\n[17] - SMTP Email Spoofer\n")
 
 #if statements
 if selection_item == "1":
@@ -27,8 +29,8 @@ if selection_item == "1":
     
         
 elif selection_item == "2":
+    print("[!]Starting...")
     #command: nmap -sP -PI -PT <ip>/24
-    print("[!] Mapping network")
     mapnIP = raw_input("What is your ip?: ")
     print("[!] Mapping Network")
     mapN_cmd = "sudo nmap -sP -PI -PT " + mapnIP + "/24"
@@ -101,7 +103,65 @@ elif selection_item == "11":
     time.sleep(1)
     print("install those scripts from nmap.org scripts section, on the tab vuln")
     
+elif selection_item == "12":
+    print("[*] Installing")
+    cmdGitXero = "git clone https://github.com/LionSec/xerosploit.git"
+    gitdirscripts = raw_input("What dir would you like to install this to?: ")
+    xerodir = "sudo cd " + gitdirscripts
+    subprocess.call(xerodir, shell=True)
+    subprocess.call(cmdGitXero, shell=True)
+    
+elif selection_item == "13":
+    print("[*] Setting up...")
+    time.sleep(5)
+    gitscript = raw_input("Whats the git link to the project?: ")
+    gitinstalldir = raw_input("What dir shall vulnbyte install this to?: ")
+    gitmaster = "sudo git clone " + gitscript
+    gitdirmaster = "sudo cd "+ gitinstalldir    
+    print("[*] Tinkering...")
+    time.sleep(1)
+    subprocess.call(gitdirmaster, shell=True)
+    print("[!] Pasting")
+    time.sleep(1)
+    subprocess.call(gitmaster, shell=True)
+    
+elif selection_item == "14":
+	print("[!] Enter A Target")
+	target_ping = raw_input("Enter a target ip: ")
+	pingcmd = "ping " + target_ping
+	os.system(pingcmd)
+    
+elif selection_item == "15":
+	print("Setting up...")
+	time.sleep(1)
+	target_tracert = raw_input("[*] Target IP: ")
+	tracert_cmd = "traceroute " + target_tracert
+	subprocess.call(tracert_cmd, shell=True)
+	
+elif selection_item == "16":
+	print("[*] Starting...")
+	time.sleep(1)
+	dnscan = raw_input("TARGET: ")
+	dnscancmd = "nslookup " + dnscan
+	subprocess.call(dnscancmd, shell=True)
+	
+elif selection_item == "17":
+	server = smtplib.SMTP('mail.smtp2go.com', 2525)
+
+	#Next, log in to the server
+	username = input("What is your smtp2go username?: ")
+	password = input("What is your smtp2go password?: ")
+	server.login(username, password)
+
+	#Send the mail
+	msg = input("Input Message Here: ") # The /n separates the message from the header
+	spoof = input("What is your spoofed email?: ")
+	sender = input("What is the target email?: ")
+
+	server.sendmail(spoof, sender, msg)
+                #rec     #target
+
+	print("Email Sent.")
 else:
     print("not a valid option")
-    cmd = "sudo python " + installdir
-    subprocess.call(cmd, shell=True)
+  
